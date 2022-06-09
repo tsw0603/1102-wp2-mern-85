5dcd7091312268c78a90af7e53a0d28cd352b3f9
tsw0603 <209410785@gms.tku.edu.tw>
2022-06-09 23:21:10 +0800

    Cors -- working version

--- client_85/src/pages/TestFetchUseProxy_85.js
+++ client_85/src/pages/TestFetchUseProxy_85.js
@@ -1,23 +1,26 @@
 import axios from 'axios';
+import { useEffect } from 'react';
 
-const TestFetchUseProxy_85=async()=>{
+const TestFetchUseProxy_85=()=>{
+    const fetchData1 = async ()=>{
    const resp =await fetch('/api/data.json');
    const data_local_json =await resp.json();
    console.log('local json data',data_local_json);
-    
-
+    }
+    const fetchData2 = async ()=>{
    const response =await fetch('/api/v1');
    const data1 = await response.json();
    console.log('fetch data1',data1);
-
+    }
+    const fetchData3 = async ()=>{
    const data2=await axios.get('/api/v1');
    console.log('axios data2',data2.data);
-
-
+    }
+    const fetchData4 = async ()=>{
     const currentUser={
         
-            name:"湯士緯1",
-            email:"2094107851@gmail.com",
+            name:"湯士緯15",
+            email:"20941078515@gmail.com",
             password:"secret15"
         
     };
@@ -31,7 +34,23 @@ const TestFetchUseProxy_85=async()=>{
     } catch(err){
         console.log(err);
     }
+}
+useEffect(()=>{
+    fetchData1();
+ }, []);
 
+ useEffect(()=>{
+     fetchData2();
+ }, []);
+
+ useEffect(()=>{
+    fetchData3();
+ }, []);
+ useEffect(()=>{
+    fetchData4();
+ }, []);
+
+    return <div></div>;
 };
 
 export default TestFetchUseProxy_85;
